fe6c3a2d775cf5122ca722f4ae48950326c318fe
Lungile Mawila <lungilemawila5@gmail.com>
2025-01-01 20:30:25 +0200

    created a java script function

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // scriping 
+
+
+ducument.getElementById('convert-btn').addEventListener('click', () => {
+    const input = document.getElementById('number').value.trim();
+    const outputElement = document.getElementById('output');
+})
+
